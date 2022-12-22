bccdcdc3575f9ff1b084288be01ed8cb3d50ae61
JaquelinMorillo <jaquelinmorillo@gmail.com>
2022-12-21 21:00:45 -0300

    avances m1

--- ModelosScriptFinal.R
+++ ModelosScriptFinal.R
@@ -7,24 +7,30 @@ library(MCMCpack) # Quinn et al 2011
 library(pscl)  # Jackman 2012
 #library(anominate) # Lo et al 2013
 #library(oc) # Poole et al 2012
+library(xtable)
+library(htmlTable)
 
 #########################################
 # Carga de datos y Descriptivos
 #########################################
 
-BASE_DESCRIPTIVA <- read_xls("BASE_DESCRIPTIVA.xlsx")
-p1 <- read_excel("p1.xlsx")
+BASE_DESCRIPTIVA <- read_excel("BASE_DESCRIPTIVA.xlsx")
+p1 <- read_excel("p1b.xlsx")
 p2 <- read_excel("p2.xlsx")
 p3 <- read_excel("p3.xlsx")
 
+########################################
+#Preparo las bases para las tablas
+########################################
+lista_p1<-p1[,1:2]
 
 ########################################
 #Modelo nominate
 ########################################
 #p1
 
-nombres1 <- p1[,1]
-p1 <- p1[,2:NCOL(p1)]
+nombres1 <- p1[,2]
+p1<- p1[,3:NCOL(p1)]
 rc_p1 <- rollcall(p1,             
                       yea=1, # reduce los valores a dos grupos yea/nay
                       nay=0,
@@ -33,10 +39,52 @@ rc_p1 <- rollcall(p1,
                       legis.names=nombres1,
                       legis.data=NULL,
                       desc="periodo pre estallido")
-result_p1 <- wnominate(rc_p1, dims=2, polarity=c(35,35))
+result_p1 <- wnominate(rc_p1, dims=1, polarity=c(143))
 summary(result_p1) # el objeto results contiene la estimacion
-windows()
 plot(result_p1)
+
+#tabla
+lista_p1table<-lista_p1 [,2]
+
+tabla_p1 <-data.frame(lista_p1table,result_p1$legislators, lista_p1$apellido)
+tabla_p1 <-tabla_p1[,c(1,6,7,8)]
+tabla_p1  <-tabla_p1[order(tabla_p1$coord1D), c(1,2,3,4)]
+tabla_p1 $rank <-rank(tabla_p1$coord1D)
+
+tabla1<-xtable(tabla_p1, caption = "Tabla de estimación ideológica, periodo pre-estallido")
+htmlTable(tabla1)
+
+lista_p1graph<-lista_p1 [,1]
+tabla1b<-data.frame(lista_p1graph$apellido,result_p1$legislators)
+tabla1b <-tabla1b[,c(1,6,7,8)]
+tabla1b  <-tabla1b[order(tabla1b$coord1D), c(1,2,3,4)]
+tabla1b $rank <-rank(tabla1b$coord1D)
+tabla1b<-xtable(tabla1b, caption = "Tabla de estimación ideológica, periodo pre-estallido")
+
+#grafico
+
+Plot_p1<-ggplot(tabla1b, aes(x = coord1D, y = rank)) +
+  geom_point() +
+  geom_line()+
+  geom_text(label = tabla1b$lista_p1graph.apellido, nudge_y = 0.3, check_overlap = T) +
+  labs(y = "Ranking",
+       x = "Coordenadas",
+       title= "Estimación Ideológica periodo pre-estallido",
+       subtitle = "dimensión",
+       caption = "Línea roja: Votante mediano")+
+  geom_vline(xintercept = -0.0541298054158688, colour = "red", linetype = "dashed")+
+  theme(axis.text = element_text(size = 15),
+        axis.title= element_text(size=16,face="bold"),
+        plot.title = element_text(size = 18, face = "bold"),
+        panel.grid.major = element_line(colour = "grey70", size = 0.2),
+        panel.grid.minor = element_blank())
+
+ggsave(Plot_p1, filename = "Results/Plot_Subcom1.png",
+       dpi = 400, width = 15, height = 9)
+
+
+
+
 
 WEIGHT=(result_senado$weights[2])/(result_senado$weights[1]) # peso relativo dado a la segunda dimension
 X1 <- result_senado$legislators$coord1D   # primera dimension
